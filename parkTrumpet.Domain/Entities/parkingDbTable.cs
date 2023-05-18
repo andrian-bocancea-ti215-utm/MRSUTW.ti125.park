78be8db9464aa98677cd63de9dac1ed858d32051
Andrian Bc <andrian.bocancea@isa.utm.md>
2023-05-18 14:02:19 +0300

    Fixat unele buguri

--- parkTrumpet.Domain/Entities/parkingDbTable.cs
+++ parkTrumpet.Domain/Entities/parkingDbTable.cs
@@ -13,24 +13,16 @@ namespace parkTrumpet.Domain.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
         [Required]
         public string Name { get; set; }
-
         [Required]
         public ownerDbTable Owner { get; set; }
-
         public bool hasCamera { get; set; }
         public bool hasBarrier { get; set; }
         public bool hasLotSensor { get; set; }
-
         public int FreeTime { get; set; }
-
         public float DayPrice { get; set; }
-
         public float NightPrice { get; set; }
-
         public string MapURL { get; set; }
-        
     }
 }
